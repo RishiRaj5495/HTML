1729d3afa7a9e303dc914a0f20930023d98f7c75
Rishi Raj Chandra <9307159365raj@gmail.com>
2025-03-26 17:29:07 +0530

    Create addCSS.js

---
+++ anshuWeb/addCSS.js
@@ -0,0 +1,75 @@
+function addCSS1() {
+
+  
+  let starButton = document.querySelector('.starss1');
+  let checkbox1 = document.querySelector('.checkbox1'); // Assuming the checkbox has an id 'checkbox'
+
+  if (checkbox1.checked) {
+    starButton.classList.remove('hover1');
+      starButton.classList.add('hover');
+       
+
+    }
+  
+   else {
+    starButton.classList.remove('hover');
+    starButton.classList.add('hover1');}
+  
+}
+
+
+
+function addCSS2() {
+  let starButton = document.querySelector('.starss2');
+  let checkbox2 = document.querySelector('.checkbox2'); // Assuming the checkbox has an id 'checkbox'
+
+  if (checkbox2.checked) {
+    starButton.classList.remove('hover1');
+      starButton.classList.add('hover');}
+  
+   else {
+    starButton.classList.remove('hover');
+    starButton.classList.add('hover1');}
+  
+}
+function addCSS3() {
+  let starButton = document.querySelector('.starss3');
+  let checkbox3 = document.querySelector('.checkbox3'); // Assuming the checkbox has an id 'checkbox'
+
+  if (checkbox3.checked) {
+    starButton.classList.remove('hover1');
+      starButton.classList.add('hover');}
+  
+   else {
+    starButton.classList.remove('hover');
+    starButton.classList.add('hover1');}
+  
+}
+
+function addCSS4() {
+  let starButton = document.querySelector('.starss4');
+  let checkbox4 = document.querySelector('.checkbox4'); // Assuming the checkbox has an id 'checkbox'
+
+  if (checkbox4.checked) {
+    starButton.classList.remove('hover1');
+      starButton.classList.add('hover');}
+  
+   else {
+    starButton.classList.remove('hover');
+    starButton.classList.add('hover1');}
+  
+}
+
+function addCSS5() {
+  let starButton = document.querySelector('.starss5');
+  let checkbox5 = document.querySelector('.checkbox5'); // Assuming the checkbox has an id 'checkbox'
+
+  if (checkbox5.checked) {
+    starButton.classList.remove('hover1');
+      starButton.classList.add('hover');}
+  
+   else {
+    starButton.classList.remove('hover');
+    starButton.classList.add('hover1');}
+  
+}
